57b8a3c46696f90cd210649b7365bccff7ae5132
mliszcz <liszcz.michal@gmail.com>
2024-04-07 00:14:08 +0200

    Add Gherkin
    * Port to vial with the default keymap (6 layers),
    * Add variant for promicro_rp2040 microcontroller,
    * Reverse ROWS and COLS to match the 'USB on the left' orientation
      (default, with south-facing switches).

---
+++ keyboards/40percentclub/gherkin/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE0, 0x4D, 0xCA, 0x9D, 0xF6, 0x7E, 0x16, 0xF5}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
